6dd6e33019969f845a497254b6a36dec9a93c256
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2024-11-14 00:26:54 +0300

    v1

---
+++ lib/mycolor.dart
@@ -0,0 +1,11 @@
+class MyColor {
+  static int gridBackground=0xFFfcbf49;
+  static int gridColorTwoFour=0xFF219ebc;
+  static int fontColorTwoFour=0xFF023047;
+  static int emptyGridBackground=0xFF8ecae6 ;
+  static int gridColorEightSixtyFourTwoFiftySix=0xFF0a9396;
+  static int gridColorOneTwentyEightFiveOneTwo=0xFFf77b5f;
+  static int gridColorSixteenThirtyTwoOneZeroTwoFour=0xFF00b4d8;
+  static int gridColorWin=0xFF60d992;
+  static int transparentWhite=0x80000000;
+}
